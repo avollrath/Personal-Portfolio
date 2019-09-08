e8782bdc6365cc0b125dd8ebc942f1750a6fa780
André Vollrath <andrevollrath@Andres-MacBook-Pro.local>
2019-09-09 00:48:27 +0300

    changed font

--- js/app.js
+++ js/app.js
@@ -1,108 +1,106 @@
 const myAge = () => {
-let myBirthday = new Date("11/28/1985"); 
-let presentDate = new Date(); 
-return ((presentDate.getTime() - myBirthday.getTime()) / (1000 * 3600 * 24) / 365).toFixed(2); 
-}
+  let myBirthday = new Date("11/28/1985");
+  let presentDate = new Date();
+  return (
+    (presentDate.getTime() - myBirthday.getTime()) /
+    (1000 * 3600 * 24) /
+    365
+  ).toFixed(2);
+};
 
 document.querySelector(".age").innerHTML = myAge();
 document.querySelector(".footer-year").innerHTML = new Date().getFullYear();
 
-const toggle = document.querySelector('.toggle input')
-const menu = document.querySelector('.menu')
+const toggle = document.querySelector(".toggle input");
+const menu = document.querySelector(".menu");
 
-document.querySelector('.toggle').addEventListener('click', () => {
-  toggle.checked = !toggle.checked
-  
-})
+document.querySelector(".toggle").addEventListener("click", () => {
+  toggle.checked = !toggle.checked;
+});
 
-let  menuState = 0;
+let menuState = 0;
 
-toggle.addEventListener('click', () => {
-
-
-if (menuState == 0) {
-
-  menu.classList.toggle('menu-active')
-
-  TweenMax.set(".toggle", {
-    backgroundColor: "transparent"
-  });
-
-  TweenMax.to(".toggle", 0.4, {
-    x: -320,
-  });
-
-  TweenMax.from(".toggle", 0.6, {
-    rotation: -360,
-    delay: 0.1
-    
-  });
-
-
-  menuState = 1;
-
-  TweenMax.set(".menu-item", {
-    scale:1
-  });
-  
-  
-    TweenMax.staggerFrom(".menu-item", 0.6, {
-      scale: 0,
-      delay:0.4,
-      ease: Power4.easeOut
-    },0.2);
-
-}
-
-else {
-
-  menuState = 0;
-
-  TweenMax.staggerTo(".menu-item", 0.1, {
-    scale: 0
-  },0.1);
+const menuFunction = () => {
 
  
 
-  
+  if (menuState == 0) {
+    menu.classList.toggle("menu-active");
 
-  setTimeout(function(){ 
+    TweenMax.set(".toggle", {
+      backgroundColor: "transparent"
+    });
 
+    TweenMax.to(".toggle", 0.5, {
+      x: -320,
+      ease: Power1.easeInOut
+    });
 
+    TweenMax.from(".toggle", 0.6, {
+      rotation: -360,
+      delay: 0.1
+    });
 
-  TweenMax.to(".toggle", 0.4, {
-    x: 0,
+    menuState = 1;
 
-  });
+    TweenMax.set(".menu-item", {
+      scale: 1
+    });
 
-  TweenMax.from(".toggle", 0.6, {
-    rotation: 360,
-    delay: 0.1
-    
-  });
-    
-  menu.classList.toggle('menu-active');
+    TweenMax.staggerFrom(
+      ".menu-item",
+      0.6,
+      {
+        scale: 0,
+        delay: 0.4,
+        ease: Power4.easeOut
+      },
+      0.2
+    );
+  } else {
+    menuState = 0;
+   
 
-}, 500);
+    TweenMax.staggerTo(
+      ".menu-item",
+      0.1,
+      {
+        scale: 0
+      },
+      -0.1
+    );
 
-setTimeout(function(){ 
+    setTimeout(function() {
+      TweenMax.to(".toggle", 0.4, {
+        x: 0
+      });
 
-  TweenMax.set(".toggle", {
-    backgroundColor: "#fff"
-  });
+      TweenMax.from(".toggle", 0.6, {
+        rotation: 360,
+        delay: 0.1
+      });
 
-}, 1000);
+      menu.classList.toggle("menu-active");
+    }, 500);
 
-
+    setTimeout(function() {
+      TweenMax.set(".toggle", {
+        backgroundColor: "#fff"
+      });
+    }, 1000);
+  }
 
 }
 
 
-  
-  
-})
+toggle.addEventListener("click", menuFunction);
 
+const menuItems = document.querySelectorAll('.menu-item');
 
+menuItems.forEach(menuItem => menuItem.addEventListener("click", function () 
+
+{menuFunction();
+  toggle.checked = !toggle.checked;}));
 
 
 TweenMax.from(".home__text", 1, {
@@ -113,35 +111,37 @@ TweenMax.from(".home__text", 1, {
 });
 
 TweenMax.from(".nav-toggle", 2, {
-    x: -1400,
-    ease: Power4.easeOut,
-    delay: 3
-  });
+  x: -1400,
+  ease: Power4.easeOut,
+  delay: 3
+});
 
-  TweenMax.from(".home__arrow-container", 0.2, {
-    opacity: 0,
-    ease: Power4.easeOut,
-    delay: 6
-  });
-
-
-
-
- 
-
-
-
+TweenMax.from(".home__arrow-container", 0.2, {
+  opacity: 0,
+  ease: Power4.easeOut,
+  delay: 6
+});
 
 let coreImgTL = new TimelineMax();
-  
-coreImgTL.staggerFrom(
+
+coreImgTL
+  .staggerFrom(
     ".coreskills__img",
     1,
     { scale: 0.01, rotation: 200, ease: Power3.easeInOut },
     0.2
-  ).staggerTo(".coreskills__img", 0.4, {scale: 1.3, ease: Power2.easeOut, delay: 0.2},0.2)
-  .staggerTo(".coreskills__img", 0.2, {scale: 1.0, ease: Power4.easeOut, delay: 0.2})
-
+  )
+  .staggerTo(
+    ".coreskills__img",
+    0.3,
+    { scale: 0.8, ease: Power2.easeOut, delay: 0.2 },
+    0.3
+  )
+  .staggerTo(".coreskills__img", 0.2, {
+    scale: 1.0,
+    ease: Power4.easeOut,
+    delay: 0.2
+  });
 
 let coreHeadlineTween = TweenMax.staggerFrom(
   ".coreskills__headline",
@@ -153,14 +153,14 @@ let coreHeadlineTween = TweenMax.staggerFrom(
 let coreParagraphTween = TweenMax.staggerFrom(
   ".coreskills__paragraph",
   1,
-  { opacity:0, ease: Power4.easeInOut, delay: 0.2},
+  { opacity: 0, ease: Power4.easeInOut, delay: 0.2 },
   0.3
 );
 
 let companiesTween = TweenMax.staggerFrom(
   ".companies__logo",
   0.4,
-  { scale: 0, ease: Power1.easeInOut, delay: 0.2, force3D:false },
+  { scale: 0, ease: Power1.easeInOut, delay: 0.2, force3D: false },
   0.08
 );
 
@@ -176,58 +176,67 @@ let skillsTween = TweenMax.from(".about-me__skills", 0.8, {
 });
 
 let skillsTL = new TimelineMax();
-  
-skillsTL.add(skillsTween)
 
-.from(".about-me__skills", 0.5, {scale:0.6, ease: Power2.easeOut, delay: -0.3})
-.from(".about-me__skills", 0.2, {rotation: 10, ease: Power4.easeOut, delay: 0.2})
+skillsTL
+  .add(skillsTween)
 
-
+  .from(".about-me__skills", 0.5, {
+    scale: 0.6,
+    ease: Power2.easeOut,
+    delay: -0.3
+  })
+  .from(".about-me__skills", 0.2, {
+    rotation: 10,
+    ease: Power4.easeOut,
+    delay: 0.2
+  });
 
 let projectsTween = TweenMax.staggerFrom(
-    ".projects__project",
-    0.1,
-    { x:-300, autoAlpha:0, scale:0.4, ease: Power3.easeOut}, 0.2
-  );
+  ".projects__project",
+  0.1,
+  { x: -300, autoAlpha: 0, scale: 0.4, ease: Power3.easeOut },
+  0.2
+);
 
-  let cableLeftTween = TweenMax.from("#cable-left", 0.3, {
-    x: -1000,
-    ease: Power3.easeOut,
-    delay: 0.4
-  });
+let cableLeftTween = TweenMax.from("#cable-left", 0.3, {
+  x: -1000,
+  ease: Power3.easeOut,
+  delay: 0.4
+});
 
-  let cableRightTween = TweenMax.from("#cable-right", 0.3, {
-    x: 1000,
-    ease: Power3.easeOut,
-    delay: 0
-  });
+let cableRightTween = TweenMax.from("#cable-right", 0.3, {
+  x: 1000,
+  ease: Power3.easeOut,
+  delay: 0
+});
 
+let contactTween = TweenMax.from(".contact__form", 0.4, {
+  opacity: 0,
+  scale: 0.1,
+  ease: Power4.easeOut,
+  delay: 0.1
+});
 
-  let contactTween = TweenMax.from(".contact__form", 0.4, {
-    opacity: 0, scale: 0.1,
-    ease: Power4.easeOut,
-    delay: 0.1
-  });
+let contactTL = new TimelineMax();
 
-  let contactTL = new TimelineMax();
-  
 contactTL
-.add(cableLeftTween)
-.add(cableRightTween)
-.add(contactTween)
-.to("#cable-left", 0.2, {y:0,x:-100, ease: Power2.easeOut, delay: 0.2})
-.to("#cable-right", 0.2, {y:0,x:100, ease: Power2.easeOut, delay: -0.4})
-.from(".contact__form", 0.4, {y:300, ease: Power4.easeOut, delay: 0.2})
-.to("#cable-left, #cable-right", 0.2, {y:0,x:0, ease: Power4.easeOut, delay: 0.3})
-.to(".contact__form", 1, {css: { 'filter': 'grayscale(0%)','-webkit-filter': 'grayscale(0%)' } , ease: Power4.easeOut, delay: -0.2})
-
-
-
-
-
-
-
-
+  .add(cableLeftTween)
+  .add(cableRightTween)
+  .add(contactTween)
+  .to("#cable-left", 0.2, { y: 0, x: -100, ease: Power2.easeOut, delay: 0.2 })
+  .to("#cable-right", 0.2, { y: 0, x: 100, ease: Power2.easeOut, delay: -0.4 })
+  .from(".contact__form", 0.4, { y: 300, ease: Power4.easeOut, delay: 0.2 })
+  .to("#cable-left, #cable-right", 0.2, {
+    y: 0,
+    x: 0,
+    ease: Power4.easeOut,
+    delay: 0.3
+  })
+  .to(".contact__form", 1, {
+    css: { filter: "grayscale(0%)", "-webkit-filter": "grayscale(0%)" },
+    ease: Power4.easeOut,
+    delay: -0.2
+  });
 
 let controller = new ScrollMagic.Controller();
 
@@ -256,14 +265,13 @@ let skillsScene = new ScrollMagic.Scene({
 }).setTween(skillsTL);
 
 let projectsScene = new ScrollMagic.Scene({
-    triggerElement: ".projects", duration: "400%"
-  }).setTween(projectsTween); 
+  triggerElement: ".projects",
+  duration: "400%"
+}).setTween(projectsTween);
 
-
-  let contactScene = new ScrollMagic.Scene({
-    triggerElement: ".contact"
-  }).setTween(contactTL);
-
+let contactScene = new ScrollMagic.Scene({
+  triggerElement: ".contact"
+}).setTween(contactTL);
 
 controller.addScene([
   aboutScene,
